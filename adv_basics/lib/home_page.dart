4c0645326effe95792b8e2017911ede537cd28eb
swapnil.g <swapnilg@zeta.tech>
2024-10-19 08:36:06 +0530

    enhancing stateful widgets

--- adv_basics/lib/home_page.dart
+++ adv_basics/lib/home_page.dart
@@ -2,47 +2,43 @@ import 'package:adv_basics/app_background.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  void myFunc() {}
+  const HomePage(void Function() startQuiz, {super.key,});
 
   @override
   Widget build(BuildContext context) {
-    return AppBackground(
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 250,
-              color: const Color.fromARGB(144, 244, 255, 255),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
+            color: const Color.fromARGB(144, 244, 255, 255),
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
             ),
-            const SizedBox(
-              height: 24.0,
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
             ),
-            const Text(
-              "Learn Flutter the fun way!",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              "Start Quiz",
             ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            OutlinedButton.icon(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.white,
-              ),
-              icon: const Icon(Icons.arrow_right_alt),
-              label: const Text(
-                "Start Quiz",
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/app_background.dart';
 import 'package:adv_basics/home_page.dart';
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 
 class Quiz extends StatefulWidget {
@@ -8,19 +10,26 @@ class Quiz extends StatefulWidget {
   State<Quiz> createState() {
     return _QuizState();
   }
-
 }
 
 class _QuizState extends State<Quiz> {
-  @override
-  Widget build(BuildContext context) {
-    
-    return const MaterialApp(
-      home: Scaffold(
-        body: HomePage(),
-      ),
-    );
 
+  Widget activeScreen = const HomePage(switchScreen);
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: AppBackground(
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
 }
