d0eab1052e66e1028df09f6f7da887e5ece0ab58
Swapnil <swapnilgupta.iiith@gmail.com>
2024-11-11 09:41:29 +0530

    adding input text widget

--- expense_tracker/lib/ui/expenses.dart
+++ expense_tracker/lib/ui/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker/data/dummy_expenese.dart';
 import 'package:expense_tracker/ui/app_background.dart';
 import 'package:expense_tracker/ui/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/ui/new_expense/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -18,6 +19,15 @@ class _ExpenseState extends State<Expenses> {
   void initState() {
     activeScreen = 'start-screen';
     super.initState();
+  }
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) {
+        return NewExpense();
+      },
+    );
   }
 
   @override
@@ -38,7 +48,7 @@ class _ExpenseState extends State<Expenses> {
         title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           )
         ],
