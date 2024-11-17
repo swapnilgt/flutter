df75f11a07fbfe8a3a39b0a99deb4c60fdfdae55
Swapnil <swapnilgupta.iiith@gmail.com>
2024-11-17 22:35:16 +0530

    Adding expense bucket class

--- expense_tracker/lib/domain/models/expense.dart
+++ expense_tracker/lib/domain/models/expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/data/dummy_expenese.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
@@ -29,5 +30,28 @@ class Expense {
 
   String get formattedDate {
     return DateFormat.yMd().format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in registeredExpenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
