609e8f2b9bcda655c25cb24632b3c648499f47ba
Swapnil <swapnilgupta.iiith@gmail.com>
2024-11-17 13:08:55 +0530

    adding dropdown

--- expense_tracker/lib/ui/new_expense/new_expense.dart
+++ expense_tracker/lib/ui/new_expense/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/domain/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -16,6 +17,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.LEISURE;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -84,8 +86,37 @@ class _NewExpenseState extends State<NewExpense> {
               )
             ],
           ),
+          SizedBox(
+            height: 16,
+          ),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toString(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) return;
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
@@ -93,12 +124,6 @@ class _NewExpenseState extends State<NewExpense> {
                 },
                 child: Text('Save Expense'),
               ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Cancel'),
-              )
             ],
           )
         ],
