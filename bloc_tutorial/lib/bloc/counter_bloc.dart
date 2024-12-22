80f9de54c38767e8aaff7da5d315ebe3251f35d1
Swapnil <swapnilgupta.iiith@gmail.com>
2024-12-22 17:09:13 +0530

    Adding missing class

---
+++ bloc_tutorial/lib/bloc/counter_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+sealed class CounterEvent {}
+
+final class CounterIncremented extends CounterEvent {}
+
+final class CounterDecremented extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncremented>((event, emit) {
+      emit(state + 1);
+    });
+
+    on<CounterDecremented>((event, emit) {
+      emit(state + 1);
+    });
+  }
+}
